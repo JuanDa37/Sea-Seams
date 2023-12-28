a88c07efc4dfa8d9ab94174978000f580e68cd7a
Juan David <juandavidpinvilla@gmail.com>
2023-12-28 10:37:23 -0500

    arreglos 57

---
+++ public/js/tiendadown.js
@@ -0,0 +1,32 @@
+var container = document.getElementById('container2')
+var container2 = document.getElementById('container3')
+var container3 = document.getElementById('container4')
+
+const item  = document.getElementsByClassName('item2')
+
+container.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container2.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container3.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
