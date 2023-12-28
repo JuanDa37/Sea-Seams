0f6a13d884ea744b2d8e8879d50329035dfc5240
Juan David <juandavidpinvilla@gmail.com>
2023-12-28 14:50:40 -0500

    arreglos 70

--- index.js
+++ index.js
@@ -181,8 +181,8 @@ app.post("/stripe-checkout", async (req, res) => {
     const session = await stripeGateway.checkout.sessions.create({
         payment_method_types: ["card"],
         mode: "payment",
-        success_url: `http://localhost:3000/success`,
-        cancel_url: `http://localhost:3000/cancel`,
+        success_url: `https://sea-seams.vercel.app/success`,
+        cancel_url: `https://sea-seams.vercel.app/cancel`,
         line_items: lineItems,
         // Pedir la dirección en la página de pago
         billing_address_collection: "required",
