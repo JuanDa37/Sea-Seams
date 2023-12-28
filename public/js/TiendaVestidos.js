0c4237d5bfa5095abd21352d701d64914eb4ff34
Juan David <juandavidpinvilla@gmail.com>
2023-12-28 09:57:26 -0500

    arreglos 54

---
+++ public/js/TiendaVestidos.js
@@ -0,0 +1,52 @@
+var container = document.getElementById('container2')
+var container2 = document.getElementById('container3')
+var container3 = document.getElementById('container4')
+var container4 = document.getElementById('container5')
+var container5 = document.getElementById('container6')
+
+const item  = document.getElementsByClassName('item2')
+
+container.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container2.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container3.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container4.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
+
+container5.addEventListener('click', (e) => {
+    var clickedItem = e.target.closest('.item2');
+    if(clickedItem){
+        clickedItem.classList.toggle('clicked');
+        clickedItem.parentElement.classList.toggle('scale')
+        clickedItem.children[1].classList.toggle('rotate')
+    }
+})
